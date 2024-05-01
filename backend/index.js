ccb43d074b6b8943b0fe6f875422dfa86ea73683
olamiji-8 <olamijisotomi@gmail.com>
2024-05-01 11:45:51 +0100

    update

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const cors = require('cors');
+const dbConnect = require("./config/dbConnect")
+const PORT = process.env.PORT || 4000
+// const Router = require("./routes/authRoute")
+
+
+
+dbConnect();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
+// app.use('/', Router);
