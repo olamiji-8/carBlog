c4676cf90456528f83203b4488c9c85097f4e584
olamiji-8 <olamijisotomi@gmail.com>
2024-05-03 07:08:37 +0100

    creating the route for the blog

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage";
+import About from "./Pages/About/About";
+import Blog from "./Pages/Blog/Blog";
+import Contact from "./Pages/Contact/Contact";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
