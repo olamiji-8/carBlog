c2532f4c73007a6ce581549a13897159236bc261
olamiji-8 <olamijisotomi@gmail.com>
2024-05-03 12:10:19 +0100

    category

---
+++ backend/config/category.js
@@ -0,0 +1,28 @@
+const category  = [
+    {
+      id: 1,
+      title: "Car Reviews",
+      description: "Lorem ipsum dolor sit amet consectetur. Urna dignissim ac egeendrerit in.",
+      image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714734175/blog/c1_dmzloh.png",
+    },
+    {
+        id: 2,
+        title: "Reviewing Cars with the Best Resale Value",
+        description: "Lorem ipsum dolor sit amet consectetur. Urna dignissim ac egeendrerit in.",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714734175/blog/c2_u2eike.png",
+      },
+      {
+        id: 3,
+        title: "A Review of Cars with Advanced Infotainment Systems",
+        description: "Lorem ipsum dolor sit amet consectetur. Urna dignissim ac egeendrerit in.",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714734175/blog/c3_qdxyds.png",
+      },
+      {
+        id: 4,
+        title: "A Deep Dive into Sports Cars",
+        description: "Lorem ipsum dolor sit amet consectetur. Urna dignissim ac egeendrerit in.",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714734175/blog/c4_xplfaf.png",
+      }
+]
+
+module.exports = category;
