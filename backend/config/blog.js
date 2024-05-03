66f8efd6c100a65a34e49e2e99ff400f71ab31de
olamiji-8 <olamijisotomi@gmail.com>
2024-05-03 06:22:11 +0100

    Adding items to the list

---
+++ backend/config/blog.js
@@ -0,0 +1,74 @@
+const blog = [
+    {
+      id: 1,
+      title: "A Review of Cars with Advanced Infotainment Systems",
+      description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+      date: "Jan 10, 2024",
+      profileName:"Dasteen",
+      image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710699/blog/1_ogbzwf.png",
+      next: "Read Article",
+      link: "https://blogs.kukeat.com/food-topics-are-tricky-how-to-choose-and-work-on-them-xv/",
+    },
+    {
+        id: 2,
+        title: "Reviewing Cars with the Best Resale Value",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710697/blog/2_s3pkyd.png",
+        next: "Read Article",
+        link: "https://blogs.kukeat.com/food-topics-are-tricky-how-to-choose-and-work-on-them-xiv/",
+      },
+      {
+        id: 3,
+        title: "A Review of Cars with Advanced Infotainment Systems",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710695/blog/3_rorzox.png",
+        next: "Read Article",
+        link: "https://blogs.kukeat.com/food-topics-are-tricky-how-to-choose-and-work-on-them-iii/"
+      },
+      {
+        id: 4,
+        title: "A Deep Dive into Sports Cars",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710696/blog/4_aenbk2.png",
+        next: "Read Article",
+        link: "https://blogs.kukeat.com/food-topics-are-tricky-how-to-choose-and-work-on-them-iii/"
+      },
+      {
+        id: 5,
+        title: "A Review of Cars with Advanced Infotainment Systems",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710695/blog/5_dntgzk.png",
+        next: "Read Article",
+        link: "https://blogs.kukeat.com/food-topics-are-tricky-how-to-choose-and-work-on-them-iii/"
+      },
+      {
+        id: 6,
+        title: "Reviewing Cars with the Best Resale Value",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710693/blog/6_n6aos4.png",
+        next: "Read Article",
+        link: "https://blogs.kukeat.com/food-topics-are-tricky-how-to-choose-and-work-on-them-iii/"
+      },
+      {
+        id: 7,
+        title: "Reviewing Cars with the Best Resale Value",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710695/blog/7_bjydpr.png",
+        next: "Read Article",
+        link: "https://blogs.kukeat.com/food-topics-are-tricky-how-to-choose-and-work-on-them-iii/"
+      }
+]
+
+module.exports = blog;
