1320d91eac05dec4479206746fc652a6fb6c9ead
olamiji-8 <olamijisotomi@gmail.com>
2024-05-03 14:22:35 +0100

    Done with the routing

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,4 +1,6 @@
 const blogData = require('../config/blog');
+const blogPost = require("../config/blogPost");
+const categoryPost = require("../config/category")
 
 const getAllPosts = (req, res) => {
     res.json(blogData);
@@ -13,7 +15,27 @@ const getPostByUuid = (req, res) => {
     res.json(post);
 };
 
+const getAllBlogPosts = (req, res) => {
+    res.json(blogPost);
+};
+
+const getBlogPostByUuid = (req, res) => {
+    const postUuid = req.params.uuid;
+    const post = blogPost.find(post => post.id === postUuid);
+    if (!post) {
+        return res.status(404).json({ message: 'Post not found' });
+    }
+    res.json(post);
+};
+
+const category = (req, res) => {
+    res.json(categoryPost);
+};
+
 module.exports = {
     getAllPosts,
-    getPostByUuid
+    getPostByUuid,
+    getAllBlogPosts,
+    getBlogPostByUuid,
+    category
 };
--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -5,6 +5,9 @@ const router = Router();
 
 router.get('/posts', blogController.getAllPosts);
 router.get('/posts/:uuid', blogController.getPostByUuid);
+router.get('/blog', blogController.getAllBlogPosts);
+router.get('/blog/:uuid', blogController.getBlogPostByUuid);
+router.get('/category', blogController.category);
 
 
 module.exports = router;
