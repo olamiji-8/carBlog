4fbc905eb6202079fe279eeae563567bfb96fe25
olamiji-8 <olamijisotomi@gmail.com>
2024-05-03 12:36:27 +0100

    updating the blogPost

--- backend/config/blog.js
+++ backend/config/blog.js
@@ -1,13 +1,14 @@
 const blog = [
     {
       id: 1,
-      title: "A Review of Cars with Advanced Infotainment Systems",
-      description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+      title: "Classic Revival: Revisiting Iconic Cars Through Modern Reviews",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
       date: "Jan 10, 2024",
       profileName:"Dasteen",
       image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710699/blog/1_ogbzwf.png",
       next: "Read Article",
       link: "https://blogs.kukeat.com/food-topics-are-tricky-how-to-choose-and-work-on-them-xv/",
+  
     },
     {
         id: 2,
---
+++ backend/config/blogPost.js
@@ -0,0 +1,60 @@
+const blogPost = [
+    {
+      id: 1,
+      title: "Classic Revival: Revisiting Iconic Cars Through Modern Reviews",
+      description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+      date: "Jan 10, 2024",
+      profileName:"Dasteen",
+      image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710699/blog/1_ogbzwf.png"
+    },
+    {
+        id: 2,
+        title: "Reviewing Cars with the Best Resale Value",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710697/blog/2_s3pkyd.png"
+      },
+      {
+        id: 3,
+        title: "A Review of Cars with Advanced Infotainment Systems",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710695/blog/3_rorzox.png"
+      },
+      {
+        id: 4,
+        title: "A Deep Dive into Sports Cars",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710696/blog/4_aenbk2.png"
+      },
+      {
+        id: 5,
+        title: "A Review of Cars with Advanced Infotainment Systems",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710695/blog/5_dntgzk.png"
+      },
+      {
+        id: 6,
+        title: "Reviewing Cars with the Best Resale Value",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710693/blog/6_n6aos4.png"
+      },
+      {
+        id: 7,
+        title: "Reviewing Cars with the Best Resale Value",
+        description: "Lorem ipsum dolor sit amet consectetur. Consectetur risus quis diam hendrerit. Interdum mattis in sed diam egestas metus at duis commodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet. ommodo. Cursus quis cursus dignissim egestas sollicitudin tristique quis. Orci neque quis porttitor eu amet.",
+        date: "Jan 10, 2024",
+        profileName:"Dasteen",
+        image:"https://res.cloudinary.com/dxd1j0yzt/image/upload/v1714710695/blog/7_bjydpr.png"
+      }
+]
+
+module.exports = blogPost;
